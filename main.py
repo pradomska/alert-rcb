4512dd9d1f6f28ff3813038581ee4e741e18f5a4
Paulina Radomska <paulinaradomska10@wp.pl>
2023-04-30 16:53:12 +0200

    emoji message

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from twilio.rest import Client
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/weather"
 LATITUDE = 54.35
 LONGITUDE = 18.65
-API_KEY = "1633515eea2b90d6d7fe64d16c9367c4"
+API_KEY = "appid"
 
-ACCOUNT_SID = "ACed99dee86823f4e65269e783a7604e42"
-AUTH_TOKEN = "80fe9d2fbcbfa696ce3878282775b89f"
+ACCOUNT_SID = "account_sid"
+AUTH_TOKEN = "auth_token"
 
 weather_params = {
     "lat": LATITUDE,
@@ -19,11 +19,30 @@ response = requests.get(OWM_Endpoint, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
 condition_code = weather_data["weather"][0]["id"]
-if int(condition_code) < 700:
-    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+weather_conditions = {
+    "2": "Thunderstorm 🌩️",
+    "3": "Drizzle 🌧️",
+    "5": "Rain ☂️",
+    "6": "Snow ⛄",
+    "7": "Fog 😶‍🌫️",
+    "8": "Clouds ⛅",
+    "800": "Clear sky ☀️"
+}
+
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+if int(condition_code) == 800:
     message = client.messages.create(
-        body="It's going to rain today. Remember to bring an ☂️",
+        body=f"{weather_conditions['800']}",
         from_="+16205268968",
-        to="+48600171513"
+        to="+48*********"
+    )
+    print(message.status)
+elif str(condition_code)[0] in weather_conditions:
+    message = client.messages.create(
+        body=f"{weather_conditions[str(condition_code)[0]]}",
+        from_="+16205268968",
+        to="+48*********"
     )
     print(message.status)
